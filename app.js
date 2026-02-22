199f402a79fa862ee7967a283f1616dc1eb695bc
Hamsiga <srihamsiga.dev@gmail.com>
2026-02-22 16:52:56 +0530

    Added read file,write file, web server  example

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("My first NodeJS app for Lab 04");
+
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+const fs1 = require('fs');
+
+fs1.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
+
+
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
